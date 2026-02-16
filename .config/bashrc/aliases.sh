d6808cbf4c9c8d4ae27c63ff5736ff8b472e394f
afni <andreaferrarini.dev@gmail.com>
2026-02-16 16:17:32 +0100

    Add alias for dotfiles repository

--- .config/bashrc/aliases.sh
+++ .config/bashrc/aliases.sh
@@ -1,7 +1,14 @@
+# ls shortcuts
 alias ls='ls --color=auto'
 alias ll='ls -l'
 alias la='ls -al'
 
+# Grep output colouring
 alias grep='grep --color=auto'
 
+# Neofetch / fastfetch alias
 alias neofetch='echo && fastfetch'
+
+# .dotfiles git repository shortcut
+alias dotfiles='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
